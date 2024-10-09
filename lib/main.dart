e07df3f15b9c101cad24e5215ef825fdb3ab02d3
jashDevkar <devkarjash1054@gmail.com>
2024-10-09 22:13:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:e_learning/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Learning Online Courses App',
+      debugShowCheckedModeBanner: false,
+      // theme: Constants.lighTheme(context),
+      home: HomeScreen(),
+    );
+  }
+}
